95d2b6f15961c9d92ac5b9f9136248d661bcc093
Burak Aydoğan <aydoganburak78@gmail.com>
2023-12-17 15:12:24 +0300

    Bookmarklar için gerekli componentlere başlandı.

---
+++ src/components/mainPanel/index.jsx
@@ -0,0 +1,11 @@
+export default function MainPanel() {
+    return (
+        <div className="w-full h-[calc(100%-64px)] p-4">
+            <h1>MainPanel</h1>
+        </div>
+        // https://www.npmjs.com/package/react-draggable
+        // https://www.youtube.com/watch?v=ULr6o6-7uNI
+        // https://github.com/enaqx/awesome-react?tab=readme-ov-file#react-real-apps
+        // https://github.com/bokuweb/react-rnd?tab=readme-ov-file#Screenshot
+    );
+}
