60e8fd20e505493a8924e6cee150d47f990be448
Burak Aydoğan <aydoganburak78@gmail.com>
2023-12-17 15:12:24 +0300

    Bookmarklar için gerekli componentlere başlandı.

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,6 +4,7 @@ import useColorScheme from "~/hooks/use-color-scheme";
 import { useTheme } from "~/stores/app/hooks";
 import { useModal } from "~/stores/modal/hooks";
 import Modal from "~/modal";
+import MainPanel from "~/components/mainPanel";
 export default function Home() {
     const theme = useTheme();
     const { colorScheme } = useColorScheme();
@@ -21,6 +22,7 @@ export default function Home() {
         <div>
             {modal && <Modal />}
             <Header />
+            <MainPanel />
         </div>
     );
 }
