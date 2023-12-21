2ceb0ebe636cac7a2719e3f8c26737c113975527
Burak Aydoğan <aydoganburak78@gmail.com>
2023-12-21 23:31:47 +0300

    Grid layout oluşturuldu temel sürükle bırak işlemleri yapıldı.

--- src/components/mainPanel/item/index.jsx
+++ src/components/mainPanel/item/index.jsx
@@ -1,68 +1,628 @@
-import React from "react";
-import { Draggable } from "react-beautiful-dnd";
-import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
+import GridLayout from "react-grid-layout";
+import "../../../../node_modules/react-grid-layout/css/styles.css";
+const MainItem = () => {
+    const bookmarksData = [
+        {
+            name: "Google",
+            url: "https://www.google.com",
+            icon: "https://www.google.com/favicon.ico",
+            id: "1",
+            x: "0",
+            y: "0",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Facebook",
+            url: "https://www.facebook.com",
+            icon: "https://www.facebook.com/favicon.ico",
+            id: "2",
+            x: "1",
+            y: "0",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "YouTube",
+            url: "https://www.youtube.com",
+            icon: "https://www.youtube.com/favicon.ico",
+            id: "3",
+            x: "2",
+            y: "0",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Twitter",
+            url: "https://www.twitter.com",
+            icon: "https://www.twitter.com/favicon.ico",
+            id: "4",
+            x: "3",
+            y: "0",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Instagram",
+            url: "https://www.instagram.com",
+            icon: "https://www.instagram.com/favicon.ico",
+            id: "5",
+            x: "4",
+            y: "0",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "LinkedIn",
+            url: "https://www.linkedin.com",
+            icon: "https://www.linkedin.com/favicon.ico",
+            id: "6",
+            x: "5",
+            y: "0",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Pinterest",
+            url: "https://www.pinterest.com",
+            icon: "https://www.pinterest.com/favicon.ico",
+            id: "7",
+            x: "6",
+            y: "0",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Snapchat",
+            url: "https://www.snapchat.com",
+            icon: "https://www.snapchat.com/favicon.ico",
+            id: "8",
+            x: "7",
+            y: "0",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Tumblr",
+            url: "https://www.tumblr.com",
+            icon: "https://www.tumblr.com/favicon.ico",
+            id: "9",
+            x: "8",
+            y: "0",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Reddit",
+            url: "https://www.reddit.com",
+            icon: "https://www.reddit.com/favicon.ico",
+            id: "10",
+            x: "9",
+            y: "0",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Amazon",
+            url: "https://www.amazon.com",
+            icon: "https://www.amazon.com/favicon.ico",
+            id: "11",
+            x: "10",
+            y: "0",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Ebay",
+            url: "https://www.ebay.com",
+            icon: "https://www.ebay.com/favicon.ico",
+            id: "12",
+            x: "11",
+            y: "0",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Netflix",
+            url: "https://www.netflix.com",
+            icon: "https://www.netflix.com/favicon.ico",
+            id: "13",
+            x: "0",
+            y: "1",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Spotify",
+            url: "https://www.spotify.com",
+            icon: "https://www.spotify.com/favicon.ico",
+            id: "14",
+            x: "1",
+            y: "1",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "WhatsApp",
+            url: "https://www.whatsapp.com",
+            icon: "https://www.whatsapp.com/favicon.ico",
+            id: "15",
+            x: "2",
+            y: "1",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Twitch",
+            url: "https://www.twitch.tv",
+            icon: "https://www.twitch.tv/favicon.ico",
+            id: "16",
+            x: "3",
+            y: "1",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Yahoo",
+            url: "https://www.yahoo.com",
+            icon: "https://www.yahoo.com/favicon.ico",
+            id: "17",
+            x: "4",
+            y: "1",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Wikipedia",
+            url: "https://www.wikipedia.org",
+            icon: "https://www.wikipedia.org/favicon.ico",
+            id: "18",
+            x: "5",
+            y: "1",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Microsoft",
+            url: "https://www.microsoft.com",
+            icon: "https://www.microsoft.com/favicon.ico",
+            id: "19",
+            x: "6",
+            y: "1",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Apple",
+            url: "https://www.apple.com",
+            icon: "https://www.apple.com/favicon.ico",
+            id: "20",
+            x: "7",
+            y: "1",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "CNN",
+            url: "https://www.cnn.com",
+            icon: "https://www.cnn.com/favicon.ico",
+            id: "21",
+            x: "8",
+            y: "1",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "BBC",
+            url: "https://www.bbc.com",
+            icon: "https://www.bbc.com/favicon.ico",
+            id: "22",
+            x: "9",
+            y: "1",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Fox News",
+            url: "https://www.foxnews.com",
+            icon: "https://www.foxnews.com/favicon.ico",
+            id: "23",
+            x: "10",
+            y: "1",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "The Guardian",
+            url: "https://www.theguardian.com",
+            icon: "https://www.theguardian.com/favicon.ico",
+            id: "24",
+            x: "11",
+            y: "1",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "CNN Türk",
+            url: "https://www.cnnturk.com",
+            icon: "https://www.cnnturk.com/favicon.ico",
+            id: "25",
+            x: "0",
+            y: "2",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Hürriyet",
+            url: "https://www.hurriyet.com.tr",
+            icon: "https://www.hurriyet.com.tr/favicon.ico",
+            id: "26",
+            x: "1",
+            y: "2",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Milliyet",
+            url: "https://www.milliyet.com.tr",
+            icon: "https://www.milliyet.com.tr/favicon.ico",
+            id: "27",
+            x: "2",
+            y: "2",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Sözcü",
+            url: "https://www.sozcu.com.tr",
+            icon: "https://www.sozcu.com.tr/favicon.ico",
+            id: "28",
+            x: "3",
+            y: "2",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "NTV",
+            url: "https://www.ntv.com.tr",
+            icon: "https://www.ntv.com.tr/favicon.ico",
+            id: "29",
+            x: "4",
+            y: "2",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "TRT Haber",
+            url: "https://www.trthaber.com",
+            icon: "https://www.trthaber.com/favicon.ico",
+            id: "30",
+            x: "5",
+            y: "2",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Google News",
+            url: "https://news.google.com",
+            icon: "https://news.google.com/favicon.ico",
+            id: "31",
+            x: "6",
+            y: "2",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Yahoo News",
+            url: "https://news.yahoo.com",
+            icon: "https://news.yahoo.com/favicon.ico",
+            id: "32",
+            x: "7",
+            y: "2",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "The New York Times",
+            url: "https://www.nytimes.com",
+            icon: "https://www.nytimes.com/favicon.ico",
+            id: "33",
+            x: "8",
+            y: "2",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "The Washington Post",
+            url: "https://www.washingtonpost.com",
+            icon: "https://www.washingtonpost.com/favicon.ico",
+            id: "34",
+            x: "9",
+            y: "2",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "The Guardian",
+            url: "https://www.theguardian.com",
+            icon: "https://www.theguardian.com/favicon.ico",
+            id: "35",
+            x: "10",
+            y: "2",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Le Monde",
+            url: "https://www.lemonde.fr",
+            icon: "https://www.lemonde.fr/favicon.ico",
+            id: "36",
+            x: "11",
+            y: "2",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Amazon",
+            url: "https://www.amazon.com",
+            icon: "https://www.amazon.com/favicon.ico",
+            id: "37",
+            x: "0",
+            y: "3",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Ebay",
+            url: "https://www.ebay.com",
+            icon: "https://www.ebay.com/favicon.ico",
+            id: "38",
+            x: "1",
+            y: "3",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Walmart",
+            url: "https://www.walmart.com",
+            icon: "https://www.walmart.com/favicon.ico",
+            id: "39",
+            x: "2",
+            y: "3",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Target",
+            url: "https://www.target.com",
+            icon: "https://www.target.com/favicon.ico",
+            id: "40",
+            x: "3",
+            y: "3",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Best Buy",
+            url: "https://www.bestbuy.com",
+            icon: "https://www.bestbuy.com/favicon.ico",
+            id: "41",
+            x: "4",
+            y: "3",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Alibaba",
+            url: "https://www.alibaba.com",
+            icon: "https://www.alibaba.com/favicon.ico",
+            id: "42",
+            x: "5",
+            y: "3",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "AliExpress",
+            url: "https://www.aliexpress.com",
+            icon: "https://www.aliexpress.com/favicon.ico",
+            id: "43",
+            x: "6",
+            y: "3",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Etsy",
+            url: "https://www.etsy.com",
+            icon: "https://www.etsy.com/favicon.ico",
+            id: "44",
+            x: "7",
+            y: "3",
+            w: "1",
+            h: "1",
+        },
+        {
+            name: "Google",
+            url: "https://www.google.com",
+            icon: "https://www.google.com/favicon.ico",
+            id: "45",
+        },
+        {
+            name: "Facebook",
+            url: "https://www.facebook.com",
+            icon: "https://www.facebook.com/favicon.ico",
+            id: "46",
+        },
+        {
+            name: "YouTube",
+            url: "https://www.youtube.com",
+            icon: "https://www.youtube.com/favicon.ico",
+            id: "47",
+        },
+        {
+            name: "Amazon",
+            url: "https://www.amazon.com",
+            icon: "https://www.amazon.com/favicon.ico",
+            id: "48",
+        },
+        {
+            name: "Twitter",
+            url: "https://www.twitter.com",
+            icon: "https://www.twitter.com/favicon.ico",
+            id: "49",
+        },
+        {
+            name: "Instagram",
+            url: "https://www.instagram.com",
+            icon: "https://www.instagram.com/favicon.ico",
+            id: "50",
+        },
+        {
+            name: "LinkedIn",
+            url: "https://www.linkedin.com",
+            icon: "https://www.linkedin.com/favicon.ico",
+            id: "51",
+        },
+        {
+            name: "Netflix",
+            url: "https://www.netflix.com",
+            icon: "https://www.netflix.com/favicon.ico",
+            id: "52",
+        },
+        {
+            name: "Pinterest",
+            url: "https://www.pinterest.com",
+            icon: "https://www.pinterest.com/favicon.ico",
+            id: "53",
+        },
+        {
+            name: "Tumblr",
+            url: "https://www.tumblr.com",
+            icon: "https://www.tumblr.com/favicon.ico",
+            id: "54",
+        },
+        {
+            name: "Snapchat",
+            url: "https://www.snapchat.com",
+            icon: "https://www.snapchat.com/favicon.ico",
+            id: "55",
+        },
+        {
+            name: "Reddit",
+            url: "https://www.reddit.com",
+            icon: "https://www.reddit.com/favicon.ico",
+            id: "56",
+        },
+        {
+            name: "Wikipedia",
+            url: "https://www.wikipedia.org",
+            icon: "https://www.wikipedia.org/favicon.ico",
+            id: "57",
+        },
+        {
+            name: "Ebay",
+            url: "https://www.ebay.com",
+            icon: "https://www.ebay.com/favicon.ico",
+            id: "58",
+        },
+        {
+            name: "Yahoo",
+            url: "https://www.yahoo.com",
+            icon: "https://www.yahoo.com/favicon.ico",
+            id: "59",
+        },
+        {
+            name: "Microsoft",
+            url: "https://www.microsoft.com",
+            icon: "https://www.microsoft.com/favicon.ico",
+            id: "60",
+        },
+        {
+            name: "Apple",
+            url: "https://www.apple.com",
+            icon: "https://www.apple.com/favicon.ico",
+            id: "61",
+        },
+        {
+            name: "Twitch",
+            url: "https://www.twitch.tv",
+            icon: "https://www.twitch.tv/favicon.ico",
+            id: "62",
+        },
+        {
+            name: "WhatsApp",
+            url: "https://www.whatsapp.com",
+            icon: "https://www.whatsapp.com/favicon.ico",
+            id: "63",
+        },
+        {
+            name: "Spotify",
+            url: "https://www.spotify.com",
+            icon: "https://www.spotify.com/favicon.ico",
+            id: "64",
+        },
+        {
+            name: "Adobe",
+            url: "https://www.adobe.com",
+            icon: "https://www.adobe.com/favicon.ico",
+            id: "65",
+        },
+    ];
 
-class MainItem extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isDragging: false,
-        };
-    }
+    const [layout, setLayout] = useState([]);
 
-    handleDragStart = () => {
-        this.setState({
-            isDragging: true,
-        });
+    useEffect(() => {
+        // Boş olan yerleri dolduracak layout'u oluştur
+        const newLayout = bookmarksData.map((bookmark, index) => ({
+            i: `bookmark${bookmark.id}`,
+            x: index % 13,
+            y: Math.floor(index / 13) * 3,
+            w: 1,
+            h: 4,
+        }));
+        setLayout(newLayout);
+    }, []);
+
+    const onLayoutChange = (newLayout) => {
+        setLayout(newLayout);
     };
 
-    handleDragEnd = () => {
-        this.setState({
-            isDragging: false,
-        });
-    };
-
-    handleClick = () => {
-        const { url } = this.props;
-        if (!this.state.isDragging) {
-            window.location.href = url;
-        }
-    };
-
-    render() {
-        const { imageSrc, text, index, id } = this.props;
-
-        return (
-            <Draggable key={id} draggableId={id} index={index}>
-                {(provided, snapshot) => (
-                    <div
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        onClick={this.handleClick}
-                        className={`flex  flex-col items-center justify-between w-[120px] h-36 border-black border rounded-md m-2 p-2 cursor-move ${
-                            snapshot.isDragging && "dragging"
-                        }`}
+    return (
+        <GridLayout
+            className="layout"
+            layout={layout}
+            cols={13}
+            rowHeight={30}
+            width={window.innerWidth}
+            onLayoutChange={onLayoutChange}
+            isResizable={false}
+            maxRows={5}
+            maxCols={13}
+            style={{ margin: "2px", padding: "2px" }}
+        >
+            {bookmarksData.map((bookmark) => (
+                <div
+                    key={`bookmark${bookmark.id}`}
+                    className="flex flex-col items-center justify-between border-black border rounded-md"
+                >
+                    <a
+                        className="flex flex-col justify-between items-center w-full h-full"
+                        href={bookmark.url}
+                        rel="noopener noreferrer"
                     >
                         <img
-                            src={imageSrc}
-                            className="min-w-14 min-h-14 rounded-lg"
-                            alt="Resim"
+                            src={bookmark.icon}
+                            className="w-20 h-20"
+                            alt={bookmark.name}
                         />
-                        <p>{text}</p>
-                    </div>
-                )}
-            </Draggable>
-        );
-    }
-}
-
-MainItem.propTypes = {
-    imageSrc: PropTypes.string.isRequired,
-    url: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired,
-    index: PropTypes.number.isRequired,
-    id: PropTypes.string.isRequired,
+                        <p>{bookmark.name}</p>
+                    </a>
+                </div>
+            ))}
+        </GridLayout>
+    );
 };
 
 export default MainItem;
